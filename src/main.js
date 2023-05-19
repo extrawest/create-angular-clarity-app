baf2d8d74fa903ac33a29b8a460e4f8dbad0b1b8
Danylo Shamaiev <danylo.shamaiev@extrawest.com>
2023-05-19 12:40:08 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const execSync = require('child_process').execSync;
+
+function main() {
+  let projectName = process.argv[2];
+  if (!projectName) {
+    console.log('Please specify project name! Example: npx create-angular-clarity-app my-app');
+    process.exit(1);
+  }
+
+  console.log(`Creating new project ${projectName}...`);
+
+  // Clone Git repo
+  const projectDir = `${process.cwd()}/${projectName}`;
+  execSync(
+    `git clone https://github.com/extrawest/ng-clarity-starter.git ${projectDir} --progress`,
+    { stdio: 'inherit' }
+  );
+
+  // Run first-config.js
+  execSync(`node ${projectDir}/first-config.js`, { stdio: 'inherit' });
+  process.exit(0);
+}
+
+if (require.main === module) {
+  main();
+}
